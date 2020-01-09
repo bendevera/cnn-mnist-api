1b6a049e366c9f0c79d9a07e70f84a2e4f592000
bendevera <ben10devera@gmail.com>
2020-01-09 14:57:15 -0700

    adding fill_db file for running on prod server

---
+++ fill_db.py
@@ -0,0 +1,38 @@
+''' 
+This script is basically to run on prod servers
+where you are not able to run the build_algos.py
+script to build models, save models and then fill 
+db with model data
+'''
+
+from app.models import Algo 
+
+
+algo_data = {
+    'optimizers': ['rmsprop', 'adam'],
+    'layers': [2, 3],
+    'description': 'CNNs use a "frame"/"slide" to deduce paterns in an image or piece of data.',
+    'type': 'Neural Network'
+}
+
+def add_to_db(optimizer, layers_num):
+    model = optimizer + str(layers_num)
+    print("Adding {} to db".format(model))
+    curr = Algo.query.filter_by(title=model).first()
+    if curr is None:
+        curr= Algo(title=model, 
+                description=algo_data['description'],
+                type=algo_data['type'],
+                optimizer=optimizer,
+                layers=layers_num)
+        db.session.add(curr)
+    else:
+        curr.description = algo_data['description']
+        curr.type = algo_data['type']
+        curr.optimizer = optimizer
+        curr.layers = layers_num
+    db.session.commit()
+
+for optimizer in algo_data['optimizers']:
+        for layer in algo_data['layers']:
+            add_to_db(optimizer, layer)
